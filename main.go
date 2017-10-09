9da8a56bd9ec106ec88ef29176a8b63efb50d37f
Steven Sklar <sklarsa@gmail.com>
2017-10-09 08:53:33 -0400

    Flag validation and additional cleanup

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"math/rand"
 	"net/http"
 	"net/http/cookiejar"
+	"os"
 	"regexp"
 	"time"
-	// "github.com/gocarina/gocsv"
 )
 
 func main() {
@@ -18,27 +18,37 @@ func main() {
 	endDatePtr := flag.String("endDate", "", "End date [YYYY-MM-DD]")
 	flag.Parse()
 
+	if *tickerPtr == "" {
+		fmt.Println("Must provide a ticker")
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	const dateFmt = "2006-01-02"
+
+	startDate, err := time.Parse(dateFmt, *startDatePtr)
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	endDate, err := time.Parse(dateFmt, *endDatePtr)
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	client, err := NewClient()
 	if err != nil {
 		panic(err)
 	}
-
-	startDate, err := time.Parse(dateFmt, *startDatePtr)
-	if err != nil {
-		panic(err)
-	}
-
-	endDate, err := time.Parse(dateFmt, *endDatePtr)
-	if err != nil {
-		panic(err)
-	}
-
-	resp, err := client.GetSecurityData(*tickerPtr, startDate, endDate)
+	resp, err := client.GetSecurityDataString(*tickerPtr, startDate, endDate)
 	fmt.Println(resp)
 }
 
+// NewClient creates a new Yahoo Finance client
 func NewClient() (*Client, error) {
 	var seedTickers = []string{"AAPL", "GOOG", "MSFT"}
 	jar, err := cookiejar.New(nil)
@@ -46,11 +56,11 @@ func NewClient() (*Client, error) {
 		return nil, err
 	}
 	httpClient := &http.Client{Jar: jar}
-	c := Client{HttpClient: httpClient}
+	c := Client{HTTPClient: httpClient}
 
 	i := rand.Intn(len(seedTickers))
 	ticker := seedTickers[i]
-	crumb, err := getCrumb(c.HttpClient, ticker)
+	crumb, err := getCrumb(c.HTTPClient, ticker)
 	if err != nil {
 		return nil, err
 	}
@@ -61,24 +71,21 @@ func NewClient() (*Client, error) {
 
 }
 
+// Client is a struct that represents a Yahoo Finance client
 type Client struct {
-	Crumb      string
-	HttpClient *http.Client
+	// Crumb is sent along with each request and is needed to make successful requests directly to the historical prices endpoint
+	Crumb string
+	// HTTPClient is a persistent client used to store cookies after the initial request is sent
+	HTTPClient *http.Client
 }
 
-type Price struct {
-	Date   time.Time
-	Open   float64
-	High   float64
-	Low    float64
-	Close  float64
-	Volume float64
-}
-
-func (c *Client) GetSecurityData(ticker string, startDate, endDate time.Time) (string, error) {
+// GetSecurityDataString returns the raw response data from the yahoo endpoint.
+// This string will be CSV formatted if the request succeeds.
+// In the event of a failed request, this string will be JSON-formatted
+func (c *Client) GetSecurityDataString(ticker string, startDate, endDate time.Time) (string, error) {
 	urlFmtStr := "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%d&period2=%d&interval=1d&events=history&crumb=%s"
 	url := fmt.Sprintf(urlFmtStr, ticker, startDate.Unix(), endDate.Unix(), c.Crumb)
-	resp, err := c.HttpClient.Get(url)
+	resp, err := c.HTTPClient.Get(url)
 	if err != nil {
 		return "", err
 	}
